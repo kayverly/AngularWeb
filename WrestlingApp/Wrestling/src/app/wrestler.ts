bb68f8a90d7016cfbb170ca3184f6fb22e2c682c
kayverly <kay.laverne.3@gmail.com>
2024-12-08 11:08:27 -0500

    setup of initial components

---
+++ WrestlingApp/Wrestling/src/app/wrestler.ts
@@ -0,0 +1,12 @@
+export interface Wrestler {
+    id: number;
+  name: string;
+  homeCity: string;
+  state: string;
+  photo: string;
+  faceOrHeel: string;
+  numOfMatches: number;
+  numOfWins: number;
+  numOfLosses: number;
+  ranking: number;
+}
